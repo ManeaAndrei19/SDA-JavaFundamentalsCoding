0816e0c533c2e2905e74ae81d48d4b89c1458d69
ManeaAndrei19 <maneaandrei19@yahoo.com>
2021-02-05 21:10:52 +0200

    andrei/arrays-strings  - Task A: Maximum product of 2 integers

---
+++ src/main/java/fundamentals/oop/ArrayExercises.java
@@ -0,0 +1,36 @@
+package fundamentals.oop;
+
+public class ArrayExercises {
+
+//    Task A: Write a Java program to find maximum product of two integers in a given array of integers.
+//            Example: nums = { 2, 3, 5, 7, -7, 5, 8, -5 } => Pair is (7, 8), Maximum Product: 56
+
+
+    public void maxProduct(int[] array) {
+        int neg1 = Integer.MAX_VALUE, neg2 = Integer.MAX_VALUE, p1 = Integer.MIN_VALUE, p2 = Integer.MIN_VALUE;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < 0) {
+                if (array[i] < neg1) {
+                    neg2 = neg1;
+                    neg1 = array[i];
+                } else if (array[i] < neg2) {
+                    neg2 = array[i];
+                }
+            } else {
+                if (array[i] > p1) {
+                    p2 = p1;
+                    p1 = array[i];
+                } else if (array[i] > p2) {
+                    p2 = array[i];
+                }
+            }
+        }
+        if (p1 * p2 > neg1 * neg2) {
+            System.out.println("Pair is (" + p1 + ", " + p2 + " ) \nMaximum Product: " + p1 * p2);
+
+        } else {
+            System.out.println("Pair is (" + neg1 + ", " + neg2 + " ) \nMaximum Product: " + neg1 * neg2);
+
+        }
+    }
+}
