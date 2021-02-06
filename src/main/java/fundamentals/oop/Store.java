2db4f448b28f57eeaf8389fb1aa0017101718840
ManeaAndrei19 <maneaandrei19@yahoo.com>
2021-02-06 10:29:04 +0200

    andrei/oop  - Store&Product: added method getListOfProductsWithPriceBiggerThan

--- src/main/java/fundamentals/oop/Store.java
+++ src/main/java/fundamentals/oop/Store.java
@@ -3,15 +3,22 @@ package fundamentals.oop;
 public class Store {
     private Product[] products;
 
-    public void prouctList(double limitPrice){
+    public Product[] getListOfProductsWithPriceBiggerThan(double limitPrice) {
+        Product[] copyArray = new Product[this.products.length];
+        int index = 0;
+
         System.out.println("Show products within price range: ");
-        for(int i=0;i< getProducts().length;i++){
-//            if()
+        for (int i = 0; i < this.products.length; i++) {
+            if (this.products[i].getPrice() > limitPrice) {
+                copyArray[index++] = this.products[i];
+            }
         }
+        return copyArray;
     }
 
+
     public Product[] getProducts() {
-        return products;
+        return this.products;
     }
 
     public void setProducts(Product[] products) {
