2404103e34238b3e5c7798aa5082a836a33b9947
ManeaAndrei19 <maneaandrei19@yahoo.com>
2021-02-20 13:48:02 +0200

    main  - Java Advanced: Enums

---
+++ src/main/java/advanced/enums/Main.java
@@ -0,0 +1,33 @@
+package advanced.enums;
+
+public class Main {
+    public static void main(String[] args) {
+        LengthUnit lengthUnit = LengthUnit.CENTIMETER;
+
+        System.out.println(lengthUnit + " has value " + lengthUnit.getValue());
+
+        switch (lengthUnit){
+            case METER:
+                System.out.println("We chose METER");
+                break;
+            case CENTIMETER:
+                System.out.println("We chose CENTIMETER");
+                break;
+            case FOOT:
+                System.out.println("We chose FOOT");
+                break;
+            case INCH:
+                System.out.println("We chose INCH");
+                break;
+        }
+
+        System.out.println(lengthUnit.name());
+        System.out.println("################");
+        for (LengthUnit unit : LengthUnit.values()
+             ) {
+            System.out.println(unit);
+
+        }
+
+    }
+}
